20caf4ef72c6f55e19d20c84154315a466dcbc7e
DimitrisKu <jimis.kurtis@icloud.com>
2025-01-09 01:13:20 +0200

    Updated AndroidManifest and AppDelegate for [describe reason]

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import GoogleMaps
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBaVWJb7bSnP4Jj7xEwAt5ditUxLn1k3q0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
